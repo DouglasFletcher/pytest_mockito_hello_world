9b59ad527b0e3f6fa457d316f9c6574a2284e1bb
douglas.fletcher <douglas.fletcher@mtechlab.local>
2023-09-25 00:36:22 +0200

    master: update verify and tests

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import module1.generic_functions as generics
 
-if __name__ == '__main__':
+def run_app():
     variable1 = "some_name"
     variable2 = 1
-    generics.some_function1(variable1, variable2)
+    result = generics.some_function1(variable1, variable2)
+    return result
+
+if __name__ == '__main__':
+    result = run_app()
+    print("result[0]=%s\nresult[1]=%s" % (result[0], result[1]))
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,16 @@ from .test_context import app
 from .test_context import generics
 
 def test_main_code_coverage():
+    # data
+    exp_0 = "example"
+    exp_1 = 2
     # mocks
-    mockito.when(generics).some_function1(any, any).thenReturn(["example",2])
+    mockito.when(generics).some_function1(mockito.any, mockito.any).thenReturn([exp_0,exp_1])
     # run
-    app
+    act_result = app.run_app()
     # tests
-    #mockito.verify(generics.some_function1, times=1)
-    #mockito.verifyNoMoreInteractions(generics.some_function1)
+    assert exp_0 == act_result[0]
+    assert exp_1 == act_result[1]
+    mockito.verify(generics, times=1).some_function1(mockito.any, mockito.any)
+    mockito.verifyNoMoreInteractions(generics)
     mockito.unstub()
